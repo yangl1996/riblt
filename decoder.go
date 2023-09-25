093569f91cbe4a2cd99ba84e6f102beaeb018779
Lei Yang <lei@yangl1996.com>
2023-09-25 11:38:21 -0400

    simplify decoder

--- decoder.go
+++ decoder.go
@@ -1,21 +1,17 @@
 package riblt
 
-type receivedSymbol[T Symbol[T]] struct {
-	CodedSymbol[T]
-	dirty bool
-}
-
 type Decoder[T Symbol[T]] struct {
-	cs []receivedSymbol[T]	// coded symbols received so far
-	local codingWindow[T]
-	window codingWindow[T]	// set of the symbols that the decoder already has
-	remote codingWindow[T]
-	dirty []int
-	pending int			// number of symbols that are not pure
+	cs []CodedSymbol[T]		// coded symbols received so far
+	isDirty []bool			// if a coded symbol is in the dirty list
+	local codingWindow[T]	// set of source symbols that are exclusive to the decoder
+	window codingWindow[T]	// set of source symbols that the decoder initially has
+	remote codingWindow[T]	// set of source symbols that are exclusive to the encoder
+	dirty []int				// indices of coded symbols that have degrees -1, 0, or 1, of which we should examine the hashes
+	decoded int				// number of coded symbols that are decoded
 }
 
 func (d *Decoder[T]) Decoded() bool {
-	return d.pending == 0
+	return d.decoded == len(d.cs)
 }
 
 func (d *Decoder[T]) Local() []HashedSymbol[T] {
@@ -40,31 +36,26 @@ func (d *Decoder[T]) AddCodedSymbol(c CodedSymbol[T]) {
 	c = d.window.applyWindow(c, remove)
 	c = d.remote.applyWindow(c, remove)
 	c = d.local.applyWindow(c, add)
-	if c.Count == 0 && c.Hash == 0 {
-		// still insert the codeword in case a symbol added later causes it to become dirty
-		d.cs = append(d.cs, receivedSymbol[T]{c, false})
-		return
+	// insert the new coded symbol
+	d.cs = append(d.cs, c)
+	if c.Count <= 1 && c.Count >= -1 {
+		d.dirty = append(d.dirty, len(d.cs)-1)
+		d.isDirty = append(d.isDirty, true)
 	} else {
-		if c.Count == -1 || c.Count == 1 {
-			d.cs = append(d.cs, receivedSymbol[T]{c, true})
-			d.dirty = append(d.dirty, len(d.cs)-1)
-		} else {
-			d.cs = append(d.cs, receivedSymbol[T]{c, false})
-		}
-		d.pending += 1
-		return
+		d.isDirty = append(d.isDirty, false)
 	}
+	return
 }
 
 func (d *Decoder[T]) applyNewSymbol(t HashedSymbol[T], direction int64) randomMapping {
 	m := randomMapping{t.Hash , 0}
 	for int(m.lastIdx) < len(d.cs) {
 		cidx := int(m.lastIdx)
-		d.cs[cidx].CodedSymbol = d.cs[cidx].apply(t, direction)
-		c := d.cs[cidx]
-		if (!c.dirty) && c.Count >= -1 && c.Count <= 1 {
-			d.cs[cidx].dirty = true
+		d.cs[cidx] = d.cs[cidx].apply(t, direction)
+		cnt := d.cs[cidx].Count
+		if (!d.isDirty[cidx]) && cnt <= 1 && cnt >= -1 {
 			d.dirty = append(d.dirty, cidx)
+			d.isDirty[cidx] = true
 		}
 		m.nextIndex()
 	}
@@ -87,7 +78,7 @@ func (d *Decoder[T]) TryDecode() {
 				ns.Hash = h
 				m := d.applyNewSymbol(ns, remove)
 				d.remote.addHashedSymbolWithMapping(ns, m)
-				d.pending -= 1
+				d.decoded += 1
 			}
 		case -1:
 			h := c.Symbol.Hash()
@@ -97,17 +88,17 @@ func (d *Decoder[T]) TryDecode() {
 				ns.Hash = h
 				m := d.applyNewSymbol(ns, add)
 				d.local.addHashedSymbolWithMapping(ns, m)
-				d.pending -= 1
+				d.decoded += 1
 			}
 		case 0:
 			if c.Hash == 0 {
-				d.pending -= 1
+				d.decoded += 1
 			}
 		// One may want to add a panic here when coded symbol is not of degree
 		// 1, -1 or 0, but this may happen when a dirty coded symbol is
 		// operated on before its turn.
 		}
-		d.cs[cidx].dirty = false
+		d.isDirty[cidx] = false
 	}
 	d.dirty = d.dirty[:0]
 }
@@ -119,9 +110,12 @@ func (d *Decoder[T]) Reset() {
 	if len(d.dirty) != 0 {
 		d.dirty = d.dirty[:0]
 	}
+	if len(d.isDirty) != 0 {
+		d.isDirty = d.isDirty[:0]
+	}
 	d.local.reset()
 	d.remote.reset()
 	d.window.reset()
-	d.pending = 0
+	d.decoded = 0
 }
 
