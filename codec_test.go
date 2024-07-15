b3fc199de3582f05b408983f2e947eb92b5bfeb6
Lei Yang <lei@yangl1996.com>
2024-07-15 15:58:05 -0400

    update tests and readme

--- codec_test.go
+++ codec_test.go
@@ -1,6 +1,7 @@
 package riblt
 
 import (
+	"fmt"
 	"encoding/binary"
 	"github.com/dchest/siphash"
 	"testing"
@@ -31,56 +32,60 @@ func newTestSymbol(i uint64) testSymbol {
 }
 
 func TestEncodeAndDecode(t *testing.T) {
-	enc := Encoder[testSymbol]{}
-	dec := Decoder[testSymbol]{}
-	local := make(map[uint64]struct{})
-	remote := make(map[uint64]struct{})
+	for _, scale := range []int{1, 2, 4, 10, 100, 1000, 10000, 20000} {
+		t.Run(fmt.Sprintf("diff %d", scale * 10), func(t *testing.T) {
+			enc := Encoder[testSymbol]{}
+			dec := Decoder[testSymbol]{}
+			local := make(map[uint64]struct{})
+			remote := make(map[uint64]struct{})
 
-	var nextId uint64
-	nlocal := 50000
-	nremote := 50000
-	ncommon := 100000
-	for i := 0; i < nlocal; i++ {
-		s := newTestSymbol(nextId)
-		nextId += 1
-		dec.AddSymbol(s)
-		local[s.Hash()] = struct{}{}
-	}
-	for i := 0; i < nremote; i++ {
-		s := newTestSymbol(nextId)
-		nextId += 1
-		enc.AddSymbol(s)
-		remote[s.Hash()] = struct{}{}
-	}
-	for i := 0; i < ncommon; i++ {
-		s := newTestSymbol(nextId)
-		nextId += 1
-		enc.AddSymbol(s)
-		dec.AddSymbol(s)
-	}
+			var nextId uint64
+			nlocal := 5*scale
+			nremote := 5*scale
+			ncommon := 10*scale
+			for i := 0; i < nlocal; i++ {
+				s := newTestSymbol(nextId)
+				nextId += 1
+				dec.AddSymbol(s)
+				local[s.Hash()] = struct{}{}
+			}
+			for i := 0; i < nremote; i++ {
+				s := newTestSymbol(nextId)
+				nextId += 1
+				enc.AddSymbol(s)
+				remote[s.Hash()] = struct{}{}
+			}
+			for i := 0; i < ncommon; i++ {
+				s := newTestSymbol(nextId)
+				nextId += 1
+				enc.AddSymbol(s)
+				dec.AddSymbol(s)
+			}
 
-	ncw := 0
-	for {
-		dec.AddCodedSymbol(enc.ProduceNextCodedSymbol())
-		ncw += 1
-		dec.TryDecode()
-		if dec.Decoded() {
-			break
-		}
+			ncw := 0
+			for {
+				dec.AddCodedSymbol(enc.ProduceNextCodedSymbol())
+				ncw += 1
+				dec.TryDecode()
+				if dec.Decoded() {
+					break
+				}
+			}
+			for _, v := range dec.Remote() {
+				delete(remote, v.Hash)
+			}
+			for _, v := range dec.Local() {
+				delete(local, v.Hash)
+			}
+			if len(remote) != 0 || len(local) != 0 {
+				t.Errorf("missing symbols: %d remote and %d local", len(remote), len(local))
+			}
+			if !dec.Decoded() {
+				t.Errorf("decoder not marked as decoded")
+			}
+			t.Logf("%d coded symbols used for decoding %d diffs overhead %.4f", ncw, nlocal+nremote, float64(ncw)/float64(nlocal+nremote))
+		})
 	}
-	for _, v := range dec.Remote() {
-		delete(remote, v.Hash)
-	}
-	for _, v := range dec.Local() {
-		delete(local, v.Hash)
-	}
-	if len(remote) != 0 || len(local) != 0 {
-		t.Errorf("missing symbols: %d remote and %d local", len(remote), len(local))
-	}
-	if !dec.Decoded() {
-		t.Errorf("decoder not marked as decoded")
-	}
-	t.Logf("%d codewords until fully decoded", ncw)
 }
 
 func BenchmarkEncoding(b *testing.B) {
